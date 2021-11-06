6aa2c7dafbdbcc877a6fcf4ef9655ac204467c24
Tuwei <82108058+Zijun-Yin@users.noreply.github.com>
2021-11-06 18:56:31 +0800

    commit all

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,10 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>baby 小芳芳</h1>
-    <img src="../assets/祈福-所有生命都被尊重.svg" alt="福-所有生命都被尊重" />
+    <img
+      src="../assets/祈福-所有生命都被尊重.svg"
+      alt="福-所有生命都被尊重"
+      width="400"
+    />
   </div>
 </template>
